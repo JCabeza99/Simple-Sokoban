4ae4a3485b8a2473b56d0d6bb9bc05b0a35b31ef
OJEDA HIGUERA, DIEGO <diego.ojeda@alumnos.upm.es>
2023-05-19 16:28:24 +0200

    gui hecho sin pedir info + rutas relativas en funcion del sistema operativo

--- src/main/java/es/upm/pproject/sokoban/view/Gui.java
+++ src/main/java/es/upm/pproject/sokoban/view/Gui.java
@@ -1,5 +1,258 @@
 package es.upm.pproject.sokoban.view;
 
-public class Gui {
+
+import static java.awt.Font.PLAIN;
+import static java.awt.Font.BOLD;
+import java.awt.*;
+import java.awt.event.*;
+
+import java.util.Iterator;
+
+import javax.swing.table.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class Gui extends JFrame {
+
+	
+    char[][] nivel  = {	{'+','+','+','+','+','+','+'},
+    					{'+','w','.','.','.','.','+'},
+    					{'+','.','.','.','.','.','+'},
+    					{'+','.','*','#','.','.','+'},
+    					{'+','.','.','.','.','.','+'},
+    					{'+','.','.','.','.','.','+'},
+    					{'+','.','.','.','.','.','+'},
+    					{'+','.','.','.','.','.','+'},
+    					{'+','.','.','.','.','.','+'},
+    					{'+','+','+','+','+','+','+'}};
+    int filas = nivel.length;
+    int columnas = nivel[0].length;
+	int fil = 4;
+	int col = 4;
+	
+	char muro = '+';
+
+	private JPanel contentPane;
+	int movimientosLvl = 0;
+	int movimientosAnt = 5;
+	
+    String sSistemaOperativo = System.getProperty("os.name");
+
+    public void paint (Graphics g)
+    {
+
+        for(int i = 0; i<filas;i++) {
+        	for (int j = 0; j < columnas; j++) {
+    			if(nivel[i][j]=='w') {
+    		        fil = i;
+    		        col = j;
+    			}
+    		}
+        }
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.setColor(Color.BLACK);
+        g2d.setFont(new Font("GEORGIA",PLAIN,40));
+        g2d.drawString("SOKOBAN", 300, 100);
+        g2d.setFont(new Font("GEORGIA",BOLD,15));
+        g2d.drawString("Movimientos:", 520, 400);
+        g2d.drawString(movimientosLvl + "", 520, 440);
+        g2d.drawString("Movimientos totales", 520, 480);
+        g2d.drawString(movimientosLvl + movimientosAnt + "", 520, 520);
+        
+        Image pared;
+        Image suelo;
+        Image player;
+        Image caja;
+        Image meta;
+        
+
+        Toolkit t = Toolkit.getDefaultToolkit ();
+        if(sSistemaOperativo.equals("Linux")) { //Como las rutas relativas son distintas en windows yu linux hay que comprobar en que SO estas
+        	pared = t.getImage ("Photos/pared.jpg");
+    		suelo = t.getImage ("Photos/suelo.jpg");
+    		player = t.getImage ("Photos/player.png");
+    		caja = t.getImage ("Photos/caja.png");
+    		meta = t.getImage ("Photos/meta.png");
+        }
+        else {
+        	pared = t.getImage ("Photos\\pared.jpg");
+    		suelo = t.getImage ("Photos\\suelo.jpg");
+    		player = t.getImage ("Photos\\player.png");
+    		caja = t.getImage ("Photos\\caja.png");
+    		meta = t.getImage ("Photos\\meta.png");
+
+        }
+
+//		Image pared = t.getImage ("fotos/pared.jpg");
+//		Image suelo = t.getImage ("fotos/suelo.jpg");
+//		Image player = t.getImage ("fotos/player.png");
+//		Image caja = t.getImage ("fotos/caja.png");
+        for(int i = 0; i<filas;i++) {
+
+        	for (int j = 0; j < columnas; j++) {
+
+				if(nivel[i][j]=='+') {
+					System.out.println("a");
+			        g2d.drawImage (pared, 80 + j*40, 140 + i*40, 40, 40, this);
+				}
+				else if(nivel[i][j]=='#'){
+					g2d.drawImage (caja, 80 + j*40, 140 + i*40, 40, 40, this);
+				}
+				else if(nivel[i][j]=='*'){
+					g2d.drawImage (suelo, 80 + j*40, 140 + i*40, 40, 40, this);
+					g2d.drawImage (meta, 80 + j*40, 140 + i*40, 40, 40, this);
+				}
+				else {
+					g2d.drawImage (suelo, 80 + j*40, 140 + i*40, 40, 40, this);
+				}
+			}
+        }
+        g2d.drawImage (player,80 + col*40, 140 + fil*40, 40, 40, this);
+
+    }
     
+
+    
+    
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui frame = new Gui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Gui() {
+		
+		 Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+	    double h= screenSize.getHeight();
+	    double w= screenSize.getWidth();
+		    
+
+
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds((int)(w-700)/2, (int)(h-700)/2, 700, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	    
+		
+	    
+	    JLabel lblZ = new JLabel("Undo");
+	    lblZ.addMouseListener(new MouseAdapter() {
+	    	public void mouseClicked(MouseEvent e) {
+	    		lblZ.setText(lblZ.getText() + ".");
+	          	//manda señal a controler 4
+
+			}
+		});
+	    lblZ.setFont(new Font("Georgia", Font.PLAIN, 14));
+	    lblZ.setBounds(520, 140, 100, 40);					//margen por izquierda margen por arriba ancho y alto		
+	    lblZ.setHorizontalAlignment(JLabel.LEFT);
+	    
+	    
+	    
+	    JLabel lblRG = new JLabel("Restart game");
+	    lblRG.addMouseListener(new MouseAdapter() {
+	    	public void mouseClicked(MouseEvent e) {
+				lblRG.setText(lblRG.getText() + ".");
+	          	//manda señal a controler 5
+			}
+		});
+	    lblRG.setFont(new Font("Georgia", Font.PLAIN, 14));
+	    lblRG.setBounds(520, 180, 100, 40);					
+	    lblRG.setHorizontalAlignment(JLabel.LEFT);
+	    
+	    
+	    
+	    JLabel lblRL = new JLabel("Restart level");
+	    lblRL.addMouseListener(new MouseAdapter() {
+	    	public void mouseClicked(MouseEvent e) {
+				lblRL.setText(lblRL.getText() + ".");
+	          	//manda señal a controler 6
+			}
+		});
+	    lblRL.setFont(new Font("Georgia", Font.PLAIN, 14));
+	    lblRL.setBounds(520, 220, 100, 40);					
+	    lblRL.setHorizontalAlignment(JLabel.LEFT);
+	    
+	    
+	    
+	    JLabel lblS = new JLabel("Save");
+	    lblS.addMouseListener(new MouseAdapter() {
+	    	public void mouseClicked(MouseEvent e) {
+	    		lblS.setText(lblS.getText() + ".");
+	          	//manda señal a controler 7
+			}
+		});
+	    lblS.setFont(new Font("Georgia", Font.PLAIN, 14));
+	    lblS.setBounds(520, 260, 100, 40);					
+	    lblS.setHorizontalAlignment(JLabel.LEFT);
+	    
+	    
+	    JLabel lblL = new JLabel("Load");
+	    lblL.addMouseListener(new MouseAdapter() {
+	    	public void mouseClicked(MouseEvent e) {
+	    		lblL.setText(lblL.getText() + ".");
+	          	//manda señal a controler 7
+			}
+		});
+	    lblL.setFont(new Font("Georgia", Font.PLAIN, 14));
+	    lblL.setBounds(520, 300, 100, 40);					
+	    lblL.setHorizontalAlignment(JLabel.LEFT);
+
+	    /*
+	     * añadir elementos al panel
+	     */
+	    contentPane.add(lblZ, BorderLayout.CENTER);
+	    contentPane.add(lblRG, BorderLayout.CENTER);
+	    contentPane.add(lblRL, BorderLayout.CENTER);
+	    contentPane.add(lblS, BorderLayout.CENTER);
+	    contentPane.add(lblL, BorderLayout.CENTER);
+	    
+	    addKeyListener(new KeyAdapter() {
+	        public void keyPressed(KeyEvent e) {
+	          int keyCode = e.getKeyCode();
+	          if (keyCode == KeyEvent.VK_UP) {
+	          	System.out.println("Up Arrrow-Key is pressed!");
+	          	//manda señal a controler 0
+	          }
+	          
+	          else if (keyCode == KeyEvent.VK_DOWN) {
+	          	System.out.println("Down Arrrow-Key is pressed!");
+	          	//manda señal a controler 1
+	          }
+	          
+	          else if (keyCode == KeyEvent.VK_LEFT) {
+	          	System.out.println("Left Arrrow-Key is pressed!");
+	          	//manda señal a controler 2
+	          }
+	          
+	          else if (keyCode == KeyEvent.VK_RIGHT) {
+	          	System.out.println("Right Arrrow-Key is pressed!");
+	          	//manda señal a controler 3
+	          }
+	        }
+	      });
+	    
+
+	    
+	}
+	
+
 }
